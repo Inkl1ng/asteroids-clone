5f95ef3af5e6ba867b09ea6cc6970ff328094463
Inkl1ng <scottbot2018@gmail.com>
2023-03-27 22:25:43 -0700

    Moved player class to its own module and added a screen dimension variables

--- main.py
+++ main.py
@@ -1,48 +1,12 @@
 import pygame
 import sys
 import os
-
-class Player:
-    def __init__(self, surface, x, y, width, height):
-        self.surface = surface
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.vel = 10
-
-    def move(self):
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_a]:
-            self.x -= self.vel
-        if keys[pygame.K_d]:
-            self.x += self.vel
-        if keys[pygame.K_w]:
-            self.y -= self.vel
-        if keys[pygame.K_s]:
-            self.y += self.vel
-    
-    def out_of_bounds(self):
-        if self.x < 0:
-            self.x = SCREEN_WIDTH
-        if self.x > SCREEN_WIDTH:
-            self.x = 0
-        if self.y < 0:
-            self.y = SCREEN_HEIGHT
-        if self.y > SCREEN_HEIGHT:
-            self.y = 0
-
-    def draw(self):
-        self.move()
-        self.out_of_bounds()
-        pygame.draw.rect(self.surface, (255, 255, 255), (self.x, self.y, self.width, self.height))
+from player import Player
 
 def main():
     pygame.init()
+    os.system('cls'| 'clear')
     RUNNING = True
-    global SCREEN_WIDTH 
-    global SCREEN_HEIGHT
     SCREEN_WIDTH = 1280
     SCREEN_HEIGHT = 720
     clock = pygame.time.Clock()
