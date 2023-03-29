c18ab01ccabed083c7abd9b8b086d8f6b410adab
Inkl1ng <scottbot2018@gmail.com>
2023-03-29 11:03:10 -0700

    created projectile for shots

---
+++ projectile.py
@@ -0,0 +1,25 @@
+'''
+a module to contain the Projectile class. A projectile class is created
+whenever the player shoots.
+'''
+import pygame
+import math
+
+
+class Projectile(object):
+    def __init__(self, surface, x: float, y: float, shot_velocity: float, angle: int):
+        self.surface = surface
+        self.COLOR = (255, 0, 0)
+        self.x = x
+        self.y = y
+        self.shot_velocity = shot_velocity
+        self.angle = angle
+        self.VELOCITY = 20
+        self.RADIUS = 5
+
+    def draw(self):
+        radians = math.radians(self.angle)
+        self.x += self.VELOCITY * math.cos(radians)
+        self.y += self.VELOCITY * math.sin(radians)
+
+        pygame.draw.circle(self.surface, self.COLOR, (self.x, self.y), self.RADIUS)
